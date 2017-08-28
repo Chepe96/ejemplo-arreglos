d65287453b90fe69011e185293ef980ea095329f
José Luis <chepemtz96@gmail.com>
2017-08-28 11:08:10 -0500

    Agregar metodos inscribir y dar de baja

---
+++ Grupo.java
@@ -0,0 +1,25 @@
+public class Grupo
+{
+    private String nomMateria;
+    private Estudiante[] estudiantes;
+    
+    public Grupo(int totalEstudiantes, String nomMateria)
+    {
+        estudiantes = new Estudiante[totalEstudiantes];
+        this.nomMateria = nomMateria;
+    }
+    
+    //terminar este metodo de tarea
+    public void inscribir(Estudiante unEstudiante)
+    {
+        if(estudiantes[0] == null)
+        {
+            estudiantes[0] = unEstudiante;
+        }
+    }
+    
+    public void darBaja(int claveEstudiante)
+    {
+        //buscar estudiante con la clave dada y asignarle un null
+    }
+}
